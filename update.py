775b00df50540492ba109c49302a65b60b85a30b
syiming <syiming@umich.edu>
2019-01-19 18:27:54 -0500

    Add constructor for update; Add all the getters

--- update.py
+++ update.py
@@ -1,5 +1,7 @@
+import numpy as np
+
 class Update:
-    #Attributes:
+    # Attributes:
     #   id - int, the event id
     #   is_bid - bool, the value of self.is_bid attribute of corresponding order
     #   price - float, the value of self.price attribute of corresponding order
@@ -7,11 +9,65 @@ class Update:
     #   delta - float, the change of remaining volume
     #   timestamp - int, an int representing the time (in millisecond)
     #   reason - int, the reason of the change, 3 possible values.
-    #Methods:
+    # Methods:
     #   initializer - initialize an Update object from a 1-d numpy array OR 7 arguments (same ordering)
-    #   getters - return each attribute
-    def __init__(self):
-        pass
+    #   getters - return each attribute, name: get_<variable name>
+
+    # Input:
+    #   file - file name of the numpy array of the update information
+    #   i - read the ith entry
+    # Modifies:
+    #   setting the initial 7 arguments
+    def __init__(self, update_entry):
+        self.is_bid = update_entry[0]
+        self.price = update_entry[1]
+        self.remaining = update_entry[2]
+        self.delta = update_entry[3]
+        self.timestamp = update_entry[4]
+        self.timestampms = update_entry[5]
+        self.reason = update_entry[6]
+        self.eventId = update_entry[7]
+
+    # Returns:
+    #   is_bid
+    def get_is_bid(self):
+        return self.is_bid == 1
+
+    # Returns:
+    #   price
+    def get_price(self):
+        return self.price
+
+    # Returns:
+    #   remaining volume
+    def get_remaining(self):
+        return self.remaining
+
+    # Returns:
+    #   delta
+    def get_delta(self):
+        return self.delta
+
+    # Returns:
+    #   timestamp
+    def get_timestamp(self):
+        return self.timestamp
+
+    # Returns:
+    #   timestampms
+    def get_timestampms(self):
+        return self.timestampms
+
+    # Returns:
+    #   reason
+    def get_reason(self):
+        return self.reason
+
+    # Return:
+    #   eventId
+    def get_eventId(self):
+        return self.eventId
+
 
 '''
 Sample function comment:
