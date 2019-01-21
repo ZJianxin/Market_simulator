039ca63045800aab6ba8c1ba7b199bf3a3ad13b9
Jianxin Zhang <jianxinz@umich.edu>
2019-01-21 16:59:57 -0500

    a bug fixed in orderbook _cancel_order() method

--- orderbook.py
+++ orderbook.py
@@ -98,8 +98,9 @@ class Orderbook:
         assert (pv_pair in self.price_volume_dict.keys()
                 and len(self.price_volume_dict[pv_pair]) > 0, "INVALID PRICE_VOLUME_PAIR KEY")
         id = self.price_volume_dict[pv_pair][0]
+        order = self.order_dict[id]
         self._remove_order(id)
-        self.order_dict[id].modify(update)
+        order.modify(update)
 
     def _trade_order(self, update):
         # trade an order AT THE TOP OF ORDERBOOK. i.e either bid_list[0] or ask_list[0] will be modified.
